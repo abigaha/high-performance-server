0fbac8a75255d814c96382745ee8f0cab7c7b2d4
abigaha <3267709954@qq.com>
2026-05-19 11:45:38 +0800

    基本完成LogAppender类

--- main/logger/logger.h
+++ main/logger/logger.h
@@ -1,8 +1,10 @@
 #pragma once
 
-// #include <fstream>
+#include <fstream>
+#include <sstream>
 // #include <mutex>
 // #include <source_location>
+#include <list>
 #include <string>
 // #include <string_view>
 #include <stdint.h>
@@ -21,13 +23,13 @@ public:
   LogEvent();
 
 private:
-  const char* m_file = nullptr;  // 文件名
-  int32_t m_line = 0;            // 行号
-  uint32_t m_threadId = 0;       // 线程ID
-  uint32_t m_fiberId = 0;        // 协程ID
-  uint32_t m_elapse = 0;         // 程序启动开始到现在的毫秒数
-  uint64_t m_time = 0;           // 时间戳
-  std::string m_content;         // 日志内容
+  const char* file_ = nullptr;  // 文件名
+  int32_t line_ = 0;            // 行号
+  uint32_t threadId_ = 0;       // 线程ID
+  uint32_t fiberId_ = 0;        // 协程ID
+  uint32_t elapse_ = 0;         // 程序启动开始到现在的毫秒数
+  uint64_t time_ = 0;           // 时间戳
+  std::string content_;         // 日志内容
 };
 
 // 日志格式器
@@ -41,13 +43,17 @@ public:
 // 日志输出地
 class LogAppender {
 public:
-  typedef std::shared_ptr<LogAppender> ptr;
+  using ptr = std::shared_ptr<LogAppender>;
   virtual ~LogAppender() = default;
 
-  void log(LogLevel level, const LogEvent::ptr event);
+  virtual void log(LogLevel level, const LogEvent::ptr event) = 0;
 
-private:
-  LogLevel m_level = LogLevel::DEBUG;
+  void setFormatter(LogFormatter::ptr val) { formatter_ = val; }
+  LogFormatter::ptr getFormatter() const { return formatter_; }
+
+protected:
+  LogLevel level_ = LogLevel::DEBUG;
+  LogFormatter::ptr formatter_;
 };
 
 // 日志器
@@ -57,16 +63,42 @@ public:
   void log(LogLevel level, const LogEvent::ptr event);
   Logger(const std::string& name = "root");
 
+  void debug(const LogEvent::ptr event);
+  void info(const LogEvent::ptr event);
+  void warn(const LogEvent::ptr event);
+  void error(const LogEvent::ptr event);
+  void fatal(const LogEvent::ptr event);
+
+  void addAppender(const LogAppender::ptr appender);
+  void delAppender(const LogAppender::ptr appender);
+  LogLevel getLevel() const { return level_; }
+  void setLevel(LogLevel val) { level_ = val; }
+
 private:
-  LogLevel level = LogLevel::DEBUG;
-  std::string m_name;
-  LogAppender::ptr m_appender;
+  LogLevel level_ = LogLevel::DEBUG;
+  std::string name_;
+  std::list<LogAppender::ptr> appenders_;
 };
 
 // 输出到控制台的Appender
-class StdoutLogAppender : public LogAppender {};
+class StdoutLogAppender : public LogAppender {
+public:
+  using ptr = std::shared_ptr<StdoutLogAppender>;
+  void log(LogLevel level, const LogEvent::ptr event) override;
+};
 
 // 输出到文件的Appender
-class FileLogAppender : public LogAppender {};
+class FileLogAppender : public LogAppender {
+public:
+  using ptr = std::shared_ptr<FileLogAppender>;
+  FileLogAppender(const std::string& filename);
+  void log(LogLevel level, const LogEvent::ptr event) override;
+
+  bool reopen();
+
+private:
+  std::string filename_;
+  std::ofstream filestream_;
+};
 
 }  // namespace hps
